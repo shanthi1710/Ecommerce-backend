1d3ee77f60216b9e5acd6b5a1d2104298f955198
shanthi1710 <shanthinathshedabale17@gmail.com>
2024-06-22 14:48:53 +0530

    Update user controller to include user listing and role change functionality

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,3 +1,4 @@
+import { Role } from './../../node_modules/.prisma/client/index.d';
 import { Request, Response } from "express";
 import { addAddressSchema, updateUserSchema } from "../schema/users";
 import { prismaClient } from "../lib/db";
@@ -45,13 +46,13 @@ export default class {
     let billingAddress: Address;
     if (validteData.defaultShippingAddress) {
       try {
-        shippingAddress =await prismaClient.address.findFirstOrThrow({
+        shippingAddress = await prismaClient.address.findFirstOrThrow({
           where: {
             id: validteData.defaultShippingAddress as any,
             userId: req.user?.id,
-          }
-        })
-        if(shippingAddress.userId != req.user?.id){
+          },
+        });
+        if (shippingAddress.userId != req.user?.id) {
           throw new NotFoundException(
             "Address not found.",
             ErrorCode.ADDRESS_NOT_FOUND
@@ -62,17 +63,17 @@ export default class {
           "Address not found.",
           ErrorCode.ADDRESS_NOT_FOUND
         );
-      } 
+      }
     }
     if (validteData.defaultBillingAddress) {
       try {
-        billingAddress =await prismaClient.address.findFirstOrThrow({
+        billingAddress = await prismaClient.address.findFirstOrThrow({
           where: {
             id: validteData.defaultBillingAddress as any,
             userId: req.user?.id,
           },
-        })
-        if(billingAddress.userId != req.user?.id){
+        });
+        if (billingAddress.userId != req.user?.id) {
           throw new NotFoundException(
             "Address not found.",
             ErrorCode.ADDRESS_NOT_FOUND
@@ -83,8 +84,8 @@ export default class {
           "Address not found.",
           ErrorCode.ADDRESS_NOT_FOUND
         );
-      } 
-      if(billingAddress.userId != req.user?.id){
+      }
+      if (billingAddress.userId != req.user?.id) {
         throw new NotFoundException(
           "Address not found.",
           ErrorCode.ADDRESS_NOT_FOUND
@@ -96,10 +97,46 @@ export default class {
         id: req.user?.id,
       },
       data: {
-        ...validteData as any
-        
+        ...(validteData as any),
       },
     });
-    res.json(updatedUser); 
+    res.json(updatedUser);
+  };
+  public static listUser = async (req: Request, res: Response) => {
+    const skip = req.query.skip ? parseInt(req.query.skip as string, 10) : 0;
+    const users = await prismaClient.user.findMany({
+      skip: skip || 0,
+      take: 5,
+    });
+    res.json(users);
+  };
+  public static getUserById = async (req: Request, res: Response) => {
+    const user = await prismaClient.user.findFirst({
+      where: {
+        id: req.params.id,
+      },
+      include: {
+        adresses: true,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException("User not found.", ErrorCode.USER_NOT_FOUND);
+    }
+    res.json(user);
+  };
+  public static changeUserRole = async (req: Request, res: Response) => {
+    const user = await prismaClient.user.update({
+      where: {
+        id: req.params.id,
+      },
+      data:{
+        role:req.body.role
+      }
+      
+    });
+    if (!user) {
+      throw new NotFoundException("User not found.", ErrorCode.USER_NOT_FOUND);
+    }
+    res.json(user);
   };
 }
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import UserService from "../controllers/users.controller"
 import { errorHandler } from "../error-handler";
 import authMiddleware from "../middlewares/auth";
+import adminMiddleware from "../middlewares/admin";
 
 
 const routes:Router =Router();
@@ -9,6 +10,9 @@ const routes:Router =Router();
 routes.post('/address',[authMiddleware],errorHandler(UserService.addAddress));
 routes.delete('/address/:id',[authMiddleware],errorHandler(UserService.deleteAddress));
 routes.get('/address',[authMiddleware],errorHandler(UserService.listAddress));
-routes.put('/',[authMiddleware],errorHandler(UserService.updateUser))
+routes.put('/',[authMiddleware],errorHandler(UserService.updateUser));
+routes.put('/role/:id',[authMiddleware,adminMiddleware],errorHandler(UserService.changeUserRole));
+routes.get('/',[authMiddleware,adminMiddleware],errorHandler(UserService.listUser))
+routes.get('/:id',[authMiddleware,adminMiddleware],errorHandler(UserService.getUserById))
 export default routes;
     
